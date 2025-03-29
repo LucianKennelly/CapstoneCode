6fe6afc3899871dfe36e71025ae117336e0a4b9c
LucianKennelly <119460731+LucianKennelly@users.noreply.github.com>
2025-03-28 18:07:30 -0700

    static save and loaded profiles for path and profile

--- PredictionApp/Scripts/main.gd
+++ PredictionApp/Scripts/main.gd
@@ -3,8 +3,8 @@ class_name Main
 
 var data = preload("res://data/testNov8.csv")
 var load_data = {}
-var save_data
-var load_path
+var save_data = {}
+var load_path = "res://data/default.json"
 var CC
 var Scaling
 var Max_Speed
@@ -15,7 +15,7 @@ var Friction
 # map scene
 var map_scene: Map_Editor = preload("res://Scenes/map_edit.tscn").instantiate()
 var map_loaded = false
-
+var map_load = "res://data/data.json"
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	CC =  $Battery/CC
@@ -31,7 +31,7 @@ func _process(delta: float) -> void:
 	if !map_loaded:
 		map_scene.visible = false
 		get_tree().root.add_child(map_scene)
-		map_scene.main_ref = self
+	#	map_scene.main_ref = self
 		map_loaded = true
 
 
@@ -104,3 +104,35 @@ func _on_edit_map_pressed() -> void:
 func launch_map():
 	self.visible = false
 	map_scene.visible = true
+
+
+func _on_load_profile_pressed() -> void:
+#	SaveManager.path = load_path
+	save_data = SaveManager.read()
+	print(save_data)
+	CC.text = str(save_data.cc)
+	Acceleration.text = str(save_data.acceleration)
+	Weight.text = str(save_data.weight)
+	Friction.text = str(save_data.friction)
+	Max_Speed.text = str(save_data.max_speed)
+	Scaling.text = str(save_data.scaling)
+
+
+func _on_profile_text_changed(new_text: String) -> void:
+	load_path = new_text
+
+
+func _on_save_profile_pressed() -> void:
+#	SaveManager.path = load_path
+#	SaveManager.create_new_save()
+	load_data["acceleration"] = Acceleration.text.to_float()
+	load_data["cc"] = CC.text.to_float()
+	load_data["weight"] = Weight.text.to_float()
+	load_data["friction"] = Friction.text.to_float()
+	load_data["max_speed"] = Max_Speed.text.to_float()
+	load_data["scaling"] = Scaling.text.to_float()
+	SaveManager.savef(load_data)
+
+
+func _on_map_text_changed(new_text: String) -> void:
+	map_load = new_text
